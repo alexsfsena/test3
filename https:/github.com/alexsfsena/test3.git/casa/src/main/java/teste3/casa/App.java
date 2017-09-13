70c216650ceba8ca50b84133889d7d550caf771e
sena <alexsfsena@gmail.com>
2017-09-12 22:41:27 -0300

    commit inicial

---
+++ https:/github.com/alexsfsena/test3.git/casa/src/main/java/teste3/casa/App.java
@@ -0,0 +1,13 @@
+package teste3.casa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!222222222" );
+    }
+}
